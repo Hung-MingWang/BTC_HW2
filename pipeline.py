6f833b2681f52101057199c27768d6eb203349c5
Hung-MingWang <b12902019@csie.ntu.edu.tw>
2026-04-10 11:45:10 +0800

    csv

--- pipeline.py
+++ pipeline.py
@@ -2,6 +2,26 @@ from datetime import date
 from config import *
 from compute import *
 from db import *
+import pandas as pd
+import os
+
+def save_to_csv(date, mnav, ev, btc_nav):
+    file = "data/mnav.csv"
+
+    new_row = {
+        "date": date,
+        "mnav": mnav,
+        "ev": ev,
+        "btc_nav": btc_nav
+    }
+
+    if os.path.exists(file):
+        df = pd.read_csv(file)
+        df = pd.concat([df, pd.DataFrame([new_row])])
+    else:
+        df = pd.DataFrame([new_row])
+
+    df.to_csv(file, index=False)
 
 def run_pipeline():
     market_cap, net_debt = get_stock_data(TICKER)
@@ -13,6 +33,7 @@ def run_pipeline():
     today = str(date.today())
 
     insert_data(today, mnav, ev, btc_nav)
+    save_to_csv(today, mnav, ev, btc_nav)
 
     print(f"{today} | mNAV={mnav:.3f}")
 
