03a230d6bb2593cd29a052a4dbf657e0c6e0087f
Hung-MingWang <b12902019@csie.ntu.edu.tw>
2026-04-21 16:23:30 +0800

    modify config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-PREFERRED_STOCK = 1.1355e10   # manually updated
-BTC_HOLDINGS = 780897        # manually updated
+PREFERRED_STOCK = 1.3540e10   # manually updated
+BTC_HOLDINGS = 815061        # manually updated
 TICKER = "MSTR"
